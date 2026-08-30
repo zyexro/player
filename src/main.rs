3d12c9e684372a443325328dd8bf55654802e2e0
zyexro <zyexro@proton.me>
2026-08-30 01:27:17 +0100

    fix: pure cdylib crate (drop desktop bin) - cargo-apk2 panics with bin+cdylib

--- src/main.rs
+++
@@ -1,7 +0,0 @@
-fn main() -> eframe::Result {
-    eframe::run_native(
-        "Player",
-        eframe::NativeOptions::default(),
-        Box::new(|cc| Ok(Box::new(player::TrackPlayer::new(cc)))),
-    )
-}
